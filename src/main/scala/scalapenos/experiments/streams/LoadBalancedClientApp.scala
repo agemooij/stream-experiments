9eb1897425963073460480b71fe85a9eef314877
Age Mooij <age.mooij@gmail.com>
2016-03-17 17:22:48 +0100

    Simple load balancer

--- src/main/scala/scalapenos/experiments/streams/LoadBalancedClientApp.scala
+++ src/main/scala/scalapenos/experiments/streams/LoadBalancedClientApp.scala
@@ -13,6 +13,8 @@ import akka.http.scaladsl._
 import akka.http.scaladsl.model._
 import akka.http.scaladsl.unmarshalling._
 
+import Uri._
+
 import akka.stream._
 import akka.stream.scaladsl._
 
@@ -21,26 +23,61 @@ object LoadBalancedClientApp extends App {
   implicit val materializer = ActorMaterializer()
   implicit val dispatcher = system.dispatcher
 
-  Await.result(responseStreamFuture(5000), 1.minute)
+  Await.result(responseStreamFuture(5, List(5000, 5001)), 1.minute)
   Await.ready(Http().shutdownAllConnectionPools(), 1.second)
   Await.result(system.terminate(), 1.seconds)
 
   println(s"Done.")
 
-  private def responseStreamFuture(port: Int): Future[Done] =
-    requests()
-      .via(poolClientFlow(port))
+  private def responseStreamFuture(nrOfRequests: Int, ports: Seq[Int]): Future[Done] =
+    requests(nrOfRequests)
+      .via(loadBalancedFlow(ports))
       .runForeach {
         case (response, id) ⇒ {
           val responseAsString = Await.result(Unmarshal(response.get.entity).to[String], 1.second)
-          println(s"${id}: ${responseAsString}")
+          println(s"${id}: Response = ${responseAsString}")
         }
       }
 
-  private def requests(): Source[(HttpRequest, Int), NotUsed] = {
-    Source.single(HttpRequest(uri = "/twitter") → 42)
+  private def requests(nr: Int): Source[(HttpRequest, Int), NotUsed] = {
+    val reqs = (1 to nr).map { id ⇒
+      HttpRequest(uri = "/twitter") → id
+    }
+
+    Source(reqs)
   }
 
   private def poolClientFlow(port: Int): Flow[(HttpRequest, Int), (Try[HttpResponse], Int), Http.HostConnectionPool] =
     Http().cachedHostConnectionPool[Int](host = "localhost", port = port)
+
+  private def loadBalancedFlow(ports: Seq[Int]): Flow[(HttpRequest, Int), (Try[HttpResponse], Int), NotUsed] = {
+    val workers = ports.map { port ⇒
+      Flow[(HttpRequest, Int)]
+        .map {
+          case in @ (request, id) ⇒ {
+            println(s"${id}: Sending request to port $port")
+            in
+          }
+        }
+        .via(Http().cachedHostConnectionPool[Int](host = "localhost", port = port).mapMaterializedValue(_ ⇒ NotUsed))
+    }
+
+    balancer(workers)
+  }
+
+  private def balancer[In, Out](workers: Seq[Flow[In, Out, Any]]): Flow[In, Out, NotUsed] = {
+    import GraphDSL.Implicits._
+
+    Flow.fromGraph(GraphDSL.create() { implicit b ⇒
+      val balancer = b.add(Balance[In](workers.size, waitForAllDownstreams = true))
+      val merge = b.add(Merge[Out](workers.size))
+
+      workers.foreach { worker ⇒
+        balancer ~> worker ~> merge
+      }
+
+      FlowShape(balancer.in, merge.out)
+    })
+  }
+
 }
